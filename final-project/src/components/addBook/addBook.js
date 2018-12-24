2878a77bc03c5f4544196b6d57b58ea31d753090
MichaelCapo23 <123pie23@gmail.com>
2018-12-24 13:42:35 -0800

    made dropdown, changed state when inputs are made

--- final-project/src/components/addBook/addBook.js
+++ final-project/src/components/addBook/addBook.js
@@ -24,20 +24,30 @@ class AddBook extends Component {
         })
     };
 
+    addBook = (event) => {
+        event.preventDefault();
+        const infoObj = this.state;
+        return infoObj;
+    };
+
     render() {
         return (
             <div className={"container"}>
                 <div className={"header"}>HEADER</div>
                 <div className={"fieldsText"}>Complete all required * fields:</div>
                 <form onSubmit={this.addBook}>
-                    <input placeholder={"Course"} className={"inputs"} onChange={this.handleInput}/>
-                    <input placeholder={"*ISBN"} className={"inputs"} onChange={this.handleInput}/>
-                    <input type={"number"} placeholder={"*condition"} className={"inputs"} onChange={this.handleInput}/>
-                    <input placeholder={"Title"} className={"inputs"} onChange={this.handleInput}/>
-                    <input placeholder={"Author"} className={"inputs"} onChange={this.handleInput}/>
-                    <input placeholder={"*Seller's email"} className={"inputs"} onChange={this.handleInput}/>
-                    <input placeholder={"*Price $$$$"} className={"inputs"} onChange={this.handleInput}/>
-                    <input placeholder={"Seller's Comments"} className={"inputs last"} onChange={this.handleInput}/>
+                    <input name={"Course"} placeholder={"Course"} className={"inputs"} onChange={this.handleInput}/>
+                    <input name={"ISBN"} placeholder={"*ISBN"} className={"inputs"} onChange={this.handleInput}/>
+                    <select name={"Condition"} onChange={this.handleInput} className={"condition"}>
+                        <option value="Excellent">Excellent</option>
+                        <option value="Used">Used</option>
+                        <option value="Heavily used">Heavily used</option>
+                    </select>
+                    <input name={"Title"} placeholder={"Title"} className={"inputs"} onChange={this.handleInput}/>
+                    <input name={"Author"} placeholder={"Author"} className={"inputs"} onChange={this.handleInput}/>
+                    <input name={"Email"} placeholder={"*Seller's email"} className={"inputs"} onChange={this.handleInput}/>
+                    <input name={"Price"} placeholder={"*Price $$$$"} className={"inputs"} onChange={this.handleInput}/>
+                    <input name={"Comments"} placeholder={"Seller's Comments"} className={"inputs last"} onChange={this.handleInput}/>
                     <div className={"text"}>Upload Book Images</div>
                     <img className={"dragDrop"} src="" alt="drag and drop"/>
                     <button className={"POST"}>Post</button>
