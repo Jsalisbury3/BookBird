9ff9f516158ccee221e3c228a1a0c6973674dbb4
Kur0a5h <kuroash.esmaili@gmail.com>
2018-12-25 13:45:14 -0800

    updated landing.css:listItem, resultList, and their children

--- final-project/src/components/landing/listItem.js
+++ final-project/src/components/landing/listItem.js
@@ -3,16 +3,18 @@ import React from 'react';
 export default props => {
     const {title,edition,author,condition,course,price}=props.about;
     return (
-        <div>
-            <img src={props.images}/>
-            <div>
+        <div className='listItem'>
+            <div className='itemImageContainer'>
+            <img className='itemImage' src={props.images}/>
+            </div>
+            <div className='itemDetails'>
                 <p>Title: {title}</p>
                 <p>Edition: {edition}</p>
                 <p>Author: {author}</p>
                 <p>Condition: {condition}</p>
                 {/* <p>Course: {course}</p> */}
             </div>
-            <div>
+            <div className='itemPrice'>
                 <h3>Price:</h3>
                 <h1>${price}</h1>
             </div>
--- final-project/src/components/landing/resultList.js
+++ final-project/src/components/landing/resultList.js
@@ -3,16 +3,15 @@ import dummyData from '../../data/dummy';
 import ListItem from './listItem';
 
 export default props => {
-    // map through the array of objects from dummy and pass into listItem component as props to fill in the component
 
-    const searchResult= dummyData.map((item,index)=>{
+    const searchResults= dummyData.map((item,index)=>{
         return (
             <ListItem key={index} about={item}/>
         )
     });
     return(
         <div className='search-results-container'>
-            {searchResult}
+            {searchResults}
         </div>
     )
 }
