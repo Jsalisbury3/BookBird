5297b835a26fc0f0cb06d05afcd22a535b980207
Kur0a5h <kuroash.esmaili@gmail.com>
2018-12-26 21:35:02 -0800

    connected listItem clicked to bookInfoPage with appropriate book data

--- final-project/src/components/BookInfo/index.js
+++ final-project/src/components/BookInfo/index.js
@@ -1,33 +1,39 @@
 import React from 'react';
-import BookData from './bookData'
+import dummyData from '../../data/dummy';
+import BookInfo from './bookData';
 
-const user =
+// const user =
 
-    {
-        id: '00',
-        title: 'biology the core',
-        ISBN: 9780134287829,
-        edition: '2nd edition',
-        author: 'Simon',
-        condition: 'used',
-        price: 35,
-        sellersComment: 'for Bio 181',
-        sellersEmail: 'jordan@sfstate.edu',
-        images: ['9780134152196.jpeg']
-    };
+//     {
+//         id: '00',
+//         title: 'biology the core',
+//         ISBN: 9780134287829,
+//         edition: '2nd edition',
+//         author: 'Simon',
+//         condition: 'used',
+//         price: 35,
+//         sellersComment: 'for Bio 181',
+//         sellersEmail: 'jordan@sfstate.edu',
+//         images: ['9780134152196.jpeg']
+//     };
 
 
-const Index = () => {
+const Index = (props) => {
+    console.log(props)
+    const id=props.match.params.bookId;
+    const bookData=dummyData.filter(value => id===value.id);
+    console.log(bookData);
+    const {title,ISBN,edition,author,condition,sellersEmail,sellersComment,price}=bookData[0];
     return (
         <div className='main-container'>
-            <BookData title={user.title}
-                      ISBN={user.ISBN}
-                      edition={user.edition}
-                      author={user.author}
-                      condition={user.condition}
-                      sellersEmail={user.sellersEmail}
-                      sellersComment={user.sellersComment}
-                      price={user.price}/>
+                    <BookInfo  title={title}
+                      ISBN={ISBN}
+                      edition={edition}
+                      author={author}
+                      condition={condition}
+                      sellersEmail={sellersEmail}
+                      sellersComment={sellersComment}
+                      price={price}/>
         </div>
     )
 };
