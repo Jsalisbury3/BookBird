51e8b44cc989e96ca70a6bec131c99bee57b2adf
Justen Quirante <justenquirante@justens-imac.attlocal.net>
2018-12-29 10:13:21 -0800

    Resolved remaining conflicts for merge

--- final-project/src/components/addBook/index.js
+++ final-project/src/components/addBook/index.js
@@ -1,19 +1,4 @@
-<<<<<<< HEAD
-import React, { Component } from 'react';
 import Modal from './modal';
-
-class Post extends Component {
-    render() {
-      return (
-        <div>
-            <Modal/>
-        </div>
-      );
-    }
-  }
-  
-  export default Post;
-=======
 import React from 'react';
 import AddBook from './addBook';
 import Header from '../universal/header';
@@ -30,4 +15,4 @@ function AddBookIndex() {
 }
 
 export default AddBookIndex
->>>>>>> af4e665877a2b2c837fb5b19e7a76492c66fc2d5
+
