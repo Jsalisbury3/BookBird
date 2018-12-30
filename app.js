4888270109ace76e7cbc9ca7ede31858f7877d96
MichaelCapo23 <123pie23@gmail.com>
2018-12-30 10:31:23 -0800

    made endpoint to database to store new book listings

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ webserver.use(function(req, res, next) {
 
 webserver.use(express.static(__dirname + '/client/dist/html'));
 webserver.use(express.urlencoded({extended : false}));
+webserver.use(express.json());
 
 webserver.get('/listings', (request, response) => {
     console.log("listing running");
@@ -33,6 +34,24 @@ webserver.get('/listings', (request, response) => {
     })
 });
 
+
+webserver.post('/addListing', (request, response) => {
+    const {title, condition, ISBN, author, edition, price, comments, images} = request.body;
+    db.connect(() => {
+        console.log("connected posting");
+        const query = "INSERT INTO `listing` SET title = '" + title + "', book_condition = '" + condition + "', ISBN = '" + ISBN + "', author = '" + author + "', edition = " + edition + ", price = " + price + ", comments = '" + comments + "', images = '" + images + "'";
+        db.query(query, (err, data) => {
+            if(!err) {
+                let output = {
+                    success: true,
+                    data: data,
+                };
+            } else {
+                console.log("you dunn fuhhh up bruh");
+            }
+        });
+    })
+});
 
 
 webserver.listen(7000, () => {
