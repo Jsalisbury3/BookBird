8b7134df89683a7ed8127d2c5bd9e0ee656ab338
MichaelCapo23 <123pie23@gmail.com>
2018-12-30 22:20:07 -0800

    made checkmarks for valid user input, npm install materialize-icon

--- client/src/components/universal/nav.js
+++ client/src/components/universal/nav.js
@@ -19,9 +19,9 @@ export default () => (
         <div className={'ProfileIcon'}>
             <Link to={"/Sign"}><img alt={ProfileIcon} className={'ProfilePicture'} src={ProfileIcon}/></Link>
         </div>
-        <div className="profileButton">
-            <Link to={"/sign/signin"}><button>Profile button</button></Link>
-        </div>
+        {/*<div className="profileButton">*/}
+            {/*<Link to={"/sign/signin"}><button>Profile button</button></Link>*/}
+        {/*</div>*/}
 
 
     </footer>
