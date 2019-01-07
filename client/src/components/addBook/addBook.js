bb38c3b8ef9a989758ee821be81cc8ce4cd481f0
MichaelCapo23 <123pie23@gmail.com>
2019-01-07 10:57:20 -0800

    made tokens, fixed search and add endpoint queries

--- client/src/components/addBook/addBook.js
+++ client/src/components/addBook/addBook.js
@@ -37,99 +37,100 @@ class AddBook extends Component {
         })
     };
 
-    validateInputsFields = (event) => {
-        event.preventDefault();
-        const test = [
-            {
-                element: 'input[name=ISBN]',
-                pattern: /[0-9]{13,}/,
-                errorMessage: "Invalid ISBN number",
-                index: 0
-            },
-            {
-                element: 'input[name=condition]',
-                pattern: /^(New|Like New|Good|Worn|Thrashed)$/,
-                errorMessage: "Invalid Condition Selection",
-                index: 1
-            },
-            {
-                element: 'input[name=title]',
-                pattern: /[a-zA-Z0-9]{4,140}/,
-                errorMessage: "Invalid Title",
-                index: 2
-            },
-            {
-                element: 'input[name=author]',
-                pattern: /[a-zA-Z0-9]{4,140}/,
-                errorMessage: "Invalid Author",
-                index: 3
-            },
-            {
-                element: 'input[name=edition]',
-                pattern: /[0-9]{1,99}/,
-                errorMessage: "Whole Numbers Only",
-                index: 4
-            },
-            {
-                element: 'input[name=price]',
-                pattern: /[0-9]{1,4}/,
-                errorMessage: "Whole Numbers Only",
-                index: 5
-            },
-        ];
-
-        if(test.length === test.filter(this.validateInputAndDisplayError).length) {
-            this.addBook();
-        }
-    };
-
-    validateInputAndDisplayError = (test) => {
-        let element = test.element;
-        if(element === "input[name=condition]") {
-            var selected = document.getElementById("mySelect").selectedIndex;
-            var options = document.getElementById("mySelect").options;
-            var elementVal = options[selected].value;
-        } else {
-            var elementVal = document.querySelector(element).value;
-        }
-        console.log(elementVal);
-        let pattern = test.pattern;
-        let errorMessage = test.errorMessage;
-        let index = test.index;
-        const result = pattern.test( elementVal );
-        if( !result ){
-            if(element !== "input[name=condition]") {
-                document.getElementsByClassName("error")[index].nextElementSibling.classList.remove("visible");
-                document.querySelector(element).nextSibling.innerHTML = errorMessage;
-            } else {
-                document.getElementById("conditionError").innerHTML = errorMessage;
-                document.getElementById("conditionCheckMArk").classList.remove("visible");
-            }
-        } else {
-            if(element !== "input[name=condition]") {
-                document.getElementsByClassName("error")[index].nextElementSibling.classList.add("visible");
-                document.querySelector(element).nextSibling.innerHTML = '';
-            } else {
-                document.getElementById("conditionError").innerHTML = '';
-                document.getElementById("conditionCheckMArk").classList.add("visible");
-            }
-
-        }
-
-        document.getElementsByClassName('modalPageContainer')[0].style.display = "block";
-        return result;
-    };
-
-    // addBook = async () => {
-    //     // event.preventDefault();
-    //     console.log("state:", this.state);
-    //     let request = {...this.state};
-    //     axios({
-    //         method: 'post',
-    //         url: '/api/addListing',
-    //         data: request,
-    //     });
+    // validateInputsFields = (event) => {
+    //     event.preventDefault();
+    //     const test = [
+    //         {
+    //             element: 'input[name=ISBN]',
+    //             pattern: /[0-9]{13,}/,
+    //             errorMessage: "Invalid ISBN number",
+    //             index: 0
+    //         },
+    //         {
+    //             element: 'input[name=condition]',
+    //             pattern: /^(New|Like New|Good|Worn|Thrashed)$/,
+    //             errorMessage: "Invalid Condition Selection",
+    //             index: 1
+    //         },
+    //         {
+    //             element: 'input[name=title]',
+    //             pattern: /[a-zA-Z0-9]{4,140}/,
+    //             errorMessage: "Invalid Title",
+    //             index: 2
+    //         },
+    //         {
+    //             element: 'input[name=author]',
+    //             pattern: /[a-zA-Z0-9]{4,140}/,
+    //             errorMessage: "Invalid Author",
+    //             index: 3
+    //         },
+    //         {
+    //             element: 'input[name=edition]',
+    //             pattern: /[0-9]{1,99}/,
+    //             errorMessage: "Whole Numbers Only",
+    //             index: 4
+    //         },
+    //         {
+    //             element: 'input[name=price]',
+    //             pattern: /[0-9]{1,4}/,
+    //             errorMessage: "Whole Numbers Only",
+    //             index: 5
+    //         },
+    //     ];
+    //
+    //     if(test.length === test.filter(this.validateInputAndDisplayError).length) {
+    //         this.addBook();
+    //     }
     // };
+    //
+    // validateInputAndDisplayError = (test) => {
+    //     let element = test.element;
+    //     if(element === "input[name=condition]") {
+    //         var selected = document.getElementById("mySelect").selectedIndex;
+    //         var options = document.getElementById("mySelect").options;
+    //         var elementVal = options[selected].value;
+    //     } else {
+    //         var elementVal = document.querySelector(element).value;
+    //     }
+    //     console.log(elementVal);
+    //     let pattern = test.pattern;
+    //     let errorMessage = test.errorMessage;
+    //     let index = test.index;
+    //     const result = pattern.test( elementVal );
+    //     if( !result ){
+    //         if(element !== "input[name=condition]") {
+    //             document.getElementsByClassName("error")[index].nextElementSibling.classList.remove("visible");
+    //             document.querySelector(element).nextSibling.innerHTML = errorMessage;
+    //         } else {
+    //             document.getElementById("conditionError").innerHTML = errorMessage;
+    //             document.getElementById("conditionCheckMArk").classList.remove("visible");
+    //         }
+    //     } else {
+    //         if(element !== "input[name=condition]") {
+    //             document.getElementsByClassName("error")[index].nextElementSibling.classList.add("visible");
+    //             document.querySelector(element).nextSibling.innerHTML = '';
+    //         } else {
+    //             document.getElementById("conditionError").innerHTML = '';
+    //             document.getElementById("conditionCheckMArk").classList.add("visible");
+    //         }
+    //
+    //     }
+    //
+    //     document.getElementsByClassName('modalPageContainer')[0].style.display = "block";
+    //     return result;
+    // };
+
+    addBook = async (event) => {
+        event.preventDefault();
+        console.log("state:", this.state);
+        let request = {...this.state};
+        axios({
+            method: 'post',
+            url: '/api/addListing',
+            headers: {token: localStorage.getItem('Token')},
+            data: request,
+        });
+    };
 
     render() {
         return (
@@ -146,7 +147,7 @@ class AddBook extends Component {
                     </div>
                   </div>
                 </div>
-                <form className={'form-container'} onSubmit={this.validateInputsFields}>
+                <form className={'form-container'} onSubmit={this.addBook}>
                     <input name={"ISBN"} placeholder={"*ISBN"} className={"inputs"} onChange={this.handleInput}/>
                     <div className={"error"}></div>
                     <div className={"checkMark markISBN material-icons"}>check_circle_outline</div>
