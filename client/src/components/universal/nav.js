3c4f59a9d3d4fae44e7b649e0eaad407a071946e
MichaelCapo23 <123pie23@gmail.com>
2019-01-09 20:31:21 -0800

    took out console logs and fix sign out endpoint bug

--- client/src/components/universal/nav.js
+++ client/src/components/universal/nav.js
@@ -23,6 +23,5 @@ export default () => (
             <Link to={"/UserProfile"}><img alt={ProfileIcon} className={'ProfilePicture'} src={ProfileIcon}/></Link>
         </div>
     </footer>
-
 )
  
--- client/src/components/user-profile/profile.js
+++ client/src/components/user-profile/profile.js
@@ -18,6 +18,7 @@ export default class UserProfile extends Component {
             headers: {token: localStorage.getItem('Token')},
         }).then((response) => {
             localStorage.clear();
+
         })
     };
 
