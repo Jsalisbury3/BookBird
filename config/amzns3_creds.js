bc55c17d0601099a0eb1f281dbcf685efe9f0943
jquirante <jcquirante@gmail.com>
2019-01-13 19:26:41 -0800

    Delete amzns3_creds.js

--- config/amzns3_creds.js
+++
@@ -1,7 +0,0 @@
-const amazon_creds = {
-    accessKeyId: 'AKIAIV7TYGGZK7MK5S3Q',
-    secretAccessKey: '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',
-    region: 'us-east-2'
-}
-
-module.exports = amazon_creds;
