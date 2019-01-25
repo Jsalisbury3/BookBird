0e107f160ce420d3de8f2ac704588eb46764d694
MichaelCapo23 <123pie23@gmail.com>
2019-01-24 23:12:27 -0800

    quick fixes

--- client/src/components/universal/nav.js
+++ client/src/components/universal/nav.js
@@ -5,21 +5,15 @@ import AddBookImg from './images/addBook.png'
 import ProfileIcon from './images/msn-people-person-profile-user-icon--icon-search-engine-16.png'
 import './universal.css';
 import { withRouter } from 'react-router-dom';
-import AddBook from '../addBook';
-import Search from '../landing';
 
 
 export default withRouter((props)=>{
     const dontShow = ['/'];
-    console.log('Nav Props:', props);
-    console.log('PathName: ', props.location)
-
     if(dontShow.includes(props.location.pathname)) return null;
-
     return(
         <footer className="uni-footer">
             <div className={'SearchIcon'}>
-                <Link classNameActive='active' to={"/Landing"}><img alt={'SearchIcon'} className={'SearchPicture'} src={SearchImg}/>
+                <Link to={"/Landing"}><img alt={'SearchIcon'} className={'SearchPicture'} src={SearchImg}/>
                     <h6 id='SearchTitle'>Search</h6>
                 </Link>
 
