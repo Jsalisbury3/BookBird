774b4084abbdd12026f86639aaccb3bd7e0f38d6
Quy Truong <qmtruong2@gmail.com>
2019-02-08 22:18:41 -0800

    tutorial video

--- client/src/components/intro-page/introPage.js
+++ client/src/components/intro-page/introPage.js
@@ -2,38 +2,24 @@ import React from 'react'
 import {Link} from 'react-router-dom';
 import './introPage.css'
 import bookBirdLogo from './images/introPageLogo.png'
-import iphonePhoto from './images/iphone-6-frame-png-7 (1).png';
+
 
 
 
 export default ()=> {
     return(
         <div className="row introPageContainer">
-            <div className="col s12 l12 m12  center-align logoImgContainer">
+            <div>
+            <iframe src="http://www.youtube.com/embed/9FkQEOjgLI0?autoplay=1&controls=0&loop=1&playlist=8HSr8BjcufM&amp;showinfo=0"></iframe>
+            </div>
+            <div className="col s12  m12  center-align logoImgContainer">
                 <img src={bookBirdLogo} className="introPageLogoImg"alt=""/>
             </div>
-            <div className="col l12 m12 s12 center-align introPageButtonContainer">
+            <div className="col  m12 s12 center-align introPageButtonContainer">
                 <Link to={"/Landing"}><p className="btn-large btn waves-effect blue lighten-3 "> Get Started </p> </Link>
             </div>
-            {/*<div className='iphoneImgContainer'>*/}
-                {/*<img src={iphonePhoto} className='iphonePhotoImg' alt='iphone photo'/>*/}
-            {/*</div>*/}
-            <div className='photoContainer'>
-                <video className='video' loop autoPlay>
-                    <source src="tutorialVideo.mp4" type="video/mp4"/>
-                </video>
-            </div>
 
-            {/*<video width="320" height="240" controls>*/}
-                {/*<source src="BookBirdTurotial.mp4" type="video/mp4"/>*/}
-            {/*</video>*/}
 
-            {/*<video src='BookBirdTurotial.mp4'/>*/}
-            {/*<div>*/}
-                {/*<video className='videoTag'>*/}
-                    {/*<source src={tutorialVideo} type='video/mp4'/>*/}
-                {/*</video>*/}
-            {/*</div>*/}
 
         </div>
     )
