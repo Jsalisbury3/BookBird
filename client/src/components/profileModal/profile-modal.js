7db2d8375e436110392edbf53bf4df3930cf2ffb
Jordan Salisbury <salisburyjordan21@gmail.com>
2019-02-24 19:15:56 -0800

    polish

--- client/src/components/profileModal/profile-modal.js
+++ client/src/components/profileModal/profile-modal.js
@@ -17,6 +17,11 @@ class ProfileModal extends Component {
 
     close = () => this.setState({isOpen: false});
 
+    deleteAndClose=() => {
+        this.props.delete()
+        this.close()
+    }
+
     render(){
         console.log(this.props)
 
@@ -26,7 +31,7 @@ class ProfileModal extends Component {
                     <div className="profile-modal-content center-align">
                         <p className="deleteTitle">Are you sure you want to delete this post?</p>
                         <button className="noButton btn green"onClick={this.close}>No</button>
-                        <button className="yesButton btn red"onClick={this.props.delete}>Yes</button>
+                        <button className="yesButton btn red" onClick={this.deleteAndClose}>Yes</button>
                     </div>                       
                 </div> 
             )
